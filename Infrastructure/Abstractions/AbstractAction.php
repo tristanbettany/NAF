4e31f0d8deba448379947e493f09c997c305a9a5
Tristan Bettany <tristanbettany@users.noreply.github.com>
2024-03-12 14:53:24 +0000

    Update AbstractAction.php

--- Infrastructure/Abstractions/AbstractAction.php
+++ Infrastructure/Abstractions/AbstractAction.php
@@ -31,4 +31,40 @@ abstract class AbstractAction implements ActionInterface
     ): ResponseInterface {
         return $responder($this->request, $data);
     }
+
+    protected function paginate(
+        string $queryParam,
+        int $itemsPerPage,
+        int $totalItems,
+        array $queryParams
+    ): array {
+        if ($totalItems !== 0) {
+            $pageCount = ceil($totalItems / $itemsPerPage);
+        } else {
+            $pageCount = 0;
+        }
+
+        if (empty($queryParams[$queryParam]) === true) {
+            $currentPage = 0; // First Page
+        } else {
+            $currentPage = $queryParams[$queryParam]; // Requested Page
+        }
+
+        if ($currentPage < 0) {
+            $currentPage = 0; // Reset to first page if negative number
+        }
+
+        if ($currentPage > $pageCount) {
+            $currentPage = $pageCount; // Reset to end of pages if higher than page count
+        }
+
+        if ($currentPage > 0) {
+            $currentPage = $currentPage - 1; // Apparently take one off if we are greater than 0 (I cant remember why)
+        }
+
+        return [
+            'currentPage' => $currentPage,
+            'pageCount' => $pageCount,
+        ];
+    }
 }
